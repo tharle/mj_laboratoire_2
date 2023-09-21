33d2f807c3c6a68e158d920598a4d45af5fecbbb
Tharle Josefi De Camargo <2330095@collegebart.ca>
2023-09-21 15:54:06 -0400

    SideView Termine

---
+++ Assets/Scripts/CubePlayerController.cs
@@ -0,0 +1,36 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubePlayerController : MonoBehaviour
+{
+
+    [SerializeField] 
+    private float m_Speed = 20f;
+
+    Rigidbody m_Rigidbody;
+
+    private void Start()
+    {
+        m_Rigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        OnBougeCube();
+    }
+
+    private void OnBougeCube()
+    {
+        var axisHorizontal = Input.GetAxis("Horizontal");
+        var axisVertical = Input.GetAxis("Vertical");
+
+        Debug.Log($" axisHorizontal {axisHorizontal}");
+        Debug.Log($" axisVertical {axisVertical}");
+        var vectorVelocity = m_Rigidbody.velocity;
+
+        vectorVelocity.x = axisHorizontal * m_Speed;
+        vectorVelocity.z = axisVertical * m_Speed;
+        m_Rigidbody.velocity = vectorVelocity;
+    }
+}
